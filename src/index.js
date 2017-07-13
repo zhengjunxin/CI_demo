fcc07beb2be805df64a31e57c73bae9f5fa59b94
郑俊鑫 <18810544291@163.com>
2017-07-13 11:18:03 +0800

    增加覆盖率的PR

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ exports.add = num => {
     else {
         return 0
     }
+
 }
-exports.min = num => {
-    if (typeof num === 'number') {
-        return num - 1
-    }
-    else {
-        return 0
-    }
-}
+exports.min = num => num - 1
